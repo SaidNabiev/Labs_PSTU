94707d54604bbd6cc5c8862e0584641b9394b618
SaidNabiev <92249418+SaidNabiev@users.noreply.github.com>
2021-10-24 22:10:44 +0500

    Homework

---
+++ 1.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int x;
+	cout << "Пожалуйста, введите трехзначное число!" << endl;
+	cin >> x;
+
+	while ((x < 100) || (x > 999))
+	{
+		cout << "Ой-ой, произошел сбой... " << '\n';
+		cout << "Число введено некорректно. Пожалуйста, введите трехзначное число, в котором нет повторяющихся цифр!" << endl;
+		cin >> x;
+	}
+	
+	int a, b, c;
+	a = x % 10;
+	x = x / 10;
+	b = x % 10;
+	c = x / 10;
+
+	while ((a == b) || (b == c) || (c == a))
+	{
+		cout << "К сожалению, цифры не могут повторяться. Вы можете попробовать еще раз!\n";
+		cin >> x;
+		a = x % 10;
+		x = x / 10;
+		b = x % 10;
+		c = x / 10;
+		
+	}
+	
+	
+	cout << "Все возможные последовательности чисел:\n";
+	
+	int t;
+	cout << a << b << c << "\t";
+	t = c; c = b; b = t;
+	cout << a << b << c << "\t";
+	t = a; a = b; b = t;
+	cout << a << b << c << "\t";
+	t = a; a = c; c = t;
+	cout << a << b << c << "\t";
+	t = c; c = b; b = t;
+	cout << a << b << c << "\t";
+	t = a; a= b; b = t;
+	cout << a << b << c << "\t";
+
+	if ((a > b) && (a > c))
+	{
+		cout << "\n\nНаибольшее число из этих чисел:";
+		if (b > c)
+		{
+			cout << a << b << c;
+
+		}
+		else
+		{
+			cout << a << c << b;
+		}
+		
+	}
+	else
+		if ((b > a) && (b > c))
+		{
+			cout << "\n\nНаибольшее число из этих чисел:";
+			if (a > c)
+			{
+				cout << b << a << c;
+			}
+			else
+			{
+
+				cout << b << c << a;
+			}
+		}
+		else
+			if ((c > b) && (c > a))
+			{
+				cout << "\n\nНаибольшее число из этих:";
+				if (b > a)
+				{
+					cout << c << b << a;
+
+				}
+				else
+				{
+					cout << c << a << b;
+				}
+			}
+	
+
+			
+
+
+	
+	
+
+	return 0;
+}
---
+++ zxc1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	srand(time(0));
+	int x, y;
+	y = -1;
+	x = rand() % 100;
+	for (int i = 1; i <= 7; i++)
+	{
+
+		while (x != y)
+		{
+			cout << "Введите число" << endl;
+			cin >> y;
+
+			if (y > x)
+			{
+				cout << "Загаданное число меньше" << endl;
+				cout << "Осталось попыток:" << 7 - i << endl;
+
+			}
+			else
+				if (y < x)
+				{
+					cout << "Загаданное число больше" << endl;
+					cout << "Осталось попыток:" << 7 - i << endl;
+
+				}
+				else
+					if (y == x)
+					{
+						cout << "Вы угадали!" << endl;
+					}
+			break;
+
+
+
+
+
+		}
+	}
+	if (y != x)
+	{
+		cout << "Вы не угадали!" << endl;
+		cout << "Задуманное число:"<<""<< x;
+	}
+
+
+	return 0;
+}
